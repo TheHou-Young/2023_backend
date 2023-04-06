269d64bda3a196aa3ab62a9c910d10b0024f292d
BK201-Drama <1243532812@qq.com>
2023-04-06 14:08:24 +0800

    chore(middlewares): rewrite middlewares

--- middlewares/translateNumber/index.js
+++ middlewares/translateNumber/index.js
@@ -1,16 +1,30 @@
 const lodash = require('lodash')
+const DEFAULT = {
+  page: 1,
+  size: 10,
+}
+
+const formStringToNumber = (value) => {
+  if (typeof value === 'string' && !lodash.isNaN(Number(value))) {
+    value = Number(value)
+  }
+}
 
 // 预处理，将get值里的字符数字转化成Number
 const translateNumber = (req, _, next) => {
   const { query } = req
   for (const key in query) {
-    if (
-      typeof query?.[key] === 'string' &&
-      !lodash.isNaN(Number(query?.[key]))
-    ) {
-      query[key] = Number(query[key])
+    formStringToNumber(query?.[key])
+    // 数组传值整理
+    if (query?.[key] instanceof Array) {
+      for (const objKey in query[key]) {
+        formStringToNumber(query[key][objKey])
+      }
     }
   }
+  // 设置默认页码
+  query.page = !lodash.isNil(query.page) ? Number(query.page) : DEFAULT.page
+  query.size = !lodash.isNil(query.size) ? Number(query.size) : DEFAULT.size
   next()
 }
 
