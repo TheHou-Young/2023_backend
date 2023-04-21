15c139a332c1e3e6cb1367dd67c97d7655c82d12
BK201-Drama <1243532812@qq.com>
2023-04-21 16:10:39 +0800

    feat(http): allow patch req

--- middlewares/httpHeader/index.js
+++ middlewares/httpHeader/index.js
@@ -1,7 +1,7 @@
 const httpHeader = (_, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With')
-  res.header('Access-Control-Allow-Methods', 'POST,GET,TRACE,OPTIONS')
+  res.header('Access-Control-Allow-Methods', 'POST,GET,TRACE,OPTIONS,PATCH,PUT')
   res.header('X-Powered-By', '3.2.1')
   next()
 }
