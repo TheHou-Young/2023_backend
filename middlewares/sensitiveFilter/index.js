c80e79e0864a2ddb963ba1d32968fa8a6f2a9c07
BK201-Drama <1243532812@qq.com>
2023-05-11 22:46:43 +0800

    feat(policy): policy word filter done

--- middlewares/sensitiveFilter/index.js
+++ middlewares/sensitiveFilter/index.js
@@ -3,14 +3,14 @@ const wordFilter = require('sensitive-word-filter')
 // 这个函数不是纯函数
 const sensitiveObjectFilter = (object, blackList = []) => {
   for (let index in object) {
-    if (blackList.includes(blackList)) continue
+    if (blackList.includes(index)) continue
     if (typeof object[index] === 'string') {
       object[index] = wordFilter.filter(object[index])
     }
   }
 }
 
-const blackList = ['password', 'account']
+const blackList = ['password', 'account', 'role_id', '_id']
 // 政治敏感词过滤器
 const sensitiveFilter = (req, _, next) => {
   // 一般在body里面才会做操作
