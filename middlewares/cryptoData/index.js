2d63f49c2647e1548492ad5a76f81541804bafae
BK201-Drama <1243532812@qq.com>
2023-05-17 14:05:20 +0800

    fix(crypto): fix when post

--- middlewares/cryptoData/index.js
+++ middlewares/cryptoData/index.js
@@ -2,10 +2,11 @@ const cryptoInstance = require('../../utils/crypto')
 const lodash = require('lodash')
 
 const decryptRequest = (req, _, next) => {
-  if (!lodash.isEmpty(req.query)) {
-    const AESKey = cryptoInstance.decryptByRSA(req.query.AESCryptoKey, process.env.RSA_PRIVATE_KEY)
-    // req.query = cryptoInstance.decryptByAES(req.query.params)
-    req.query = cryptoInstance.decryptByAES(req.query.params, AESKey)
+  const keys = !lodash.isEmpty(req.query) ? 'query' : 'body'
+  const dataKeys = !lodash.isEmpty(req.query) ? 'params' : 'data'
+  if (!lodash.isEmpty(req[keys])) {
+    const AESKey = cryptoInstance.decryptByRSA(req?.[keys].AESCryptoKey, process.env.RSA_PRIVATE_KEY)
+    req[keys] = cryptoInstance.decryptByAES(req[keys][dataKeys], AESKey)
     _.AESKey = AESKey
   }
   next()
